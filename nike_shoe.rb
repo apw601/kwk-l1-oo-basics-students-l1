848bad993cda3a9e6585cdfba2349cf346a755fc
apw601 <apw601@gmail.com>
2018-06-07 22:25:36 +0000

    automatically backed up by learn

---
+++ nike_shoe.rb
@@ -0,0 +1,6 @@
+class Shoes
+  #need to add attributes for color, laces, base, and swoosh
+  def color = (color_base) #{gets the color for the base of a shoe}
+    @this_shoes_color = color_base
+  end
+end
